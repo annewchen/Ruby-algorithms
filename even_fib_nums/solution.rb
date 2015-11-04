9f0ad984deb44e7705bb10b0b0fc4c8306f787a1
Anne Chen <annewchen@gmail.com>
2015-11-04 13:36:07 -0800

    add even_fib pseudocode

--- even_fib_nums/solution.rb
+++ even_fib_nums/solution.rb
@@ -1,2 +1,11 @@
 #Pseudocode
-#input
+#input: num - upper max to fib nums
+#output: sum of all even fib nums with upper bound of input num
+
+#Need to create an array of fibonacci nums, fib_nums, with the highest value of input num.
+#Sum all items in fib_nums
+
+#create an array, fib_nums, with elements 1 and 2.
+#sum the last 2 elements and add the value into fib_nums until the value exceeds the input num.
+#sum all the elements in the array.
+
